5b24934372653e1d55251431a9b5851d9f320faa
PhucHoangNguyen1812 <mailkphuc@gmail.com>
2021-08-20 16:50:34 +0700

    Thiết kế web bootstrap và sử dụng sitemesh decorator phân chia giao diện

--- src/main/java/com/webtintuc/controller/web/HomeController.java
+++ src/main/java/com/webtintuc/controller/web/HomeController.java
@@ -9,12 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.webtintuc.model.UserModel;
+
 @WebServlet(urlPatterns = {"/trang-chu"})
 public class HomeController extends HttpServlet {
 
 	private static final long serialVersionUID = -3713221121227713118L;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		RequestDispatcher rd =  request.getRequestDispatcher("view/web/home.jsp");
 		rd.forward(request, response);
 	}
